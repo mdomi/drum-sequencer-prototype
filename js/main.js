4d6c3daa52955d9447c9f06e61547bf6f9d01e37
Michael Dominice <michael.dominice@issinc.com>
2014-03-06 22:55:38 -0500

    initial sequencer capability complete

--- js/main.js
+++ js/main.js
@@ -3,25 +3,41 @@
 
     var document = window.document,
         MS_PER_S = 1000,
-        S_PER_MIN = 60;
+        S_PER_MIN = 60,
+        slice = Array.prototype.slice;
 
     function timePerBeat(bpm) {
         return MS_PER_S * S_PER_MIN / bpm;
     }
 
     function times(x, fn) {
-        var results = [], i = 0, args = Array.prototype.slice.call(arguments, 2);
+        var results = [], i = 0, args = slice.call(arguments, 2);
         for (i = 0; i < x; i++) {
             results.push(fn.apply(null, args));
         }
         return results;
     }
 
+    function invoke(arr, name) {
+        var args = slice.call(arguments, 2);
+        return arr.map(function (x) {
+            return x[name].apply(x, args);
+        });
+    }
+
+    function getStorageBoolean(key) {
+        return window.localStorage[key] === 'true';
+    }
+
+    function setStorageBoolean(key, value) {
+        window.localStorage[key] = value; // will get converted into a string
+    }
+
     function Square() {
         this.el = document.createElement('span');
         this.el.classList.add('sequencer-square');
 
-        var toggled = false;
+        var isToggled = false;
 
         this.activate = function () {
             this.el.classList.add('sequencer-square-active');
@@ -33,16 +49,17 @@
 
         this.toggle = function () {
             this.el.classList.toggle('sequencer-square-toggle');
-            toggled = !toggled;
+            isToggled = !isToggled;
         }.bind(this);
 
         this.toggled = function () {
-            return toggled;
+            return isToggled;
         };
 
         this.bindEvents = function () {
             this.el.addEventListener('click', this.toggle, false);
         };
+
     }
 
     Square.create = function () {
@@ -60,11 +77,15 @@
         return new Date().getTime();
     }
 
-    function SequencerRow(width) {
+    function SequencerRow(width, context) {
         this.el = document.createElement('div');
 
         var squares = [],
-            active;
+            active,
+            input = document.createElement('input'),
+            buffer;
+
+        input.type = 'file';
 
         squares = times(width, Square.create);
 
@@ -72,9 +93,23 @@
             this.el.appendChild(square.el);
         }.bind(this));
 
+        this.el.appendChild(input);
+
+        function play() {
+            if (buffer) {
+                var source = context.createBufferSource();
+                source.buffer = buffer;
+                source.connect(context.destination);
+                source.noteOn(0);
+            }
+        }
+
         this.bindEvents = function () {
-            squares.forEach(function (square) {
-                square.bindEvents();
+            invoke(squares, 'bindEvents');
+            input.addEventListener('change', function () {
+                readAudioFile(input.files[0], context, function (audioBuffer) {
+                    buffer = audioBuffer;
+                });
             });
         };
 
@@ -83,7 +118,12 @@
                 active.deactivate();
             }
             active = squares[index];
-            active.activate();
+            if (active) {
+                active.activate();
+                if (active.toggled()) {
+                    play();
+                }
+            }
         };
 
         this.clearActive = function () {
@@ -95,8 +135,8 @@
 
     }
 
-    SequencerRow.create = function (width) {
-        return new SequencerRow(width);
+    SequencerRow.create = function (width, context) {
+        return new SequencerRow(width, context);
     };
 
     function SequencerController(height) {
@@ -105,6 +145,7 @@
 
         var display = document.createElement('span'),
             tempo = 108,
+            context = new webkitAudioContext(),
             timeout,
             currentBeat,
             length = 16,
@@ -114,7 +155,7 @@
                 step.call(this);
                 timeout = window.setTimeout(schedule, Math.floor(next - (timestamp() - now)));
             }.bind(this),
-            rows = times(height, SequencerRow.create, length);
+            rows = times(height, SequencerRow.create, length, context);
 
         this.el.appendChild(display);
         rows.forEach(function (row) {
@@ -122,13 +163,13 @@
         }.bind(this));
 
         this.bindEvents = function () {
-            rows.forEach(function (row) {
-                row.bindEvents();
-            });
+            invoke(rows, 'bindEvents');
         };
 
         function setup() {
             currentBeat = 0;
+            invoke(rows, 'setActive');
+            updateDisplay();
         }
 
         function updateDisplay() {
@@ -150,23 +191,58 @@
 
         this.start = function () {
             this.stop();
-            setup();
             schedule();
         };
 
         this.stop = function () {
+            setup();
             if (timeout) {
                 window.clearTimeout(timeout);
             }
         };
     }
 
+    function targetMatchesClass(event, className) {
+        return event.target.classList.contains(className);
+    }
 
-    var controller = new SequencerController(3);
+    function readFile(file, cb) {
+        var reader = new FileReader();
+
+        reader.onload = function () {
+            cb(reader.result);
+        };
+
+        reader.readAsArrayBuffer(file);
+    }
+
+    function readAudioFile(file, context, cb) {
+        readFile(file, function (arrayBuffer) {
+            context.decodeAudioData(arrayBuffer, cb);
+        });
+    }
+
+    function ControlsController(el, sequencer) {
+        this.el = el;
+
+        this.bindEvents = function () {
+            this.el.addEventListener('click', function (event) {
+                if (targetMatchesClass(event, 'js-play')) {
+                    sequencer.start();
+                } else if (targetMatchesClass(event, 'js-stop')) {
+                    sequencer.stop();
+                }
+            });
+        };
+    }
+
+
+    var controller = new SequencerController(3),
+        controls = new ControlsController(
+            document.getElementById('controls'), controller);
 
     document.getElementById('main').appendChild(controller.el);
     controller.bindEvents();
-
-    controller.start();
+    controls.bindEvents();
 
 }(this));
